77c4e2911cb9ff5d39db9fbc2b8406d7e665b080
ThapananKulab <nicekrubma123@gmail.com>
2024-02-08 19:10:42 +0700

    update

--- API/server.js
+++ API/server.js
@@ -3,13 +3,13 @@ var cors = require('cors')
 var bodyParser = require('body-parser');
 var app = express();
 var jsonParser = bodyParser.json()
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-var cookieParser = require('cookie-parser')
-const mysql = require('mysql2');
+// var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const mongoose = require('mongoose');
 
+// var cookieParser = require('cookie-parser')
+// var jwt = require('jsonwebtoken');
 
 app.use(cors());
-
 
 app.post('/login', jsonParser, function (req, res) {
     const { username } = req.body;
@@ -20,30 +20,17 @@ app.post('/login', jsonParser, function (req, res) {
 });
 
 app.post('/register', jsonParser, async function (req, res, next) {
-    try {
-        const [results, fields] = await connection.execute(
-            'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
-            ['Rick C-137', 53]
-        );
-        console.log(results);
-        console.log(fields);
-        res.json({ results, fields });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+
 });
 
-
-//database
-// mongoose.Promise = global.Promise;
-// const db = mongoose.connection;
-// db.on('error',(error)=>console.log(error));
-// db.once('open',()=>console.log('Database Already'))
-// mongoose.connect('mongodb+srv://nicekrubma10:kulab12345@cluster0.uqjxafb.mongodb.net/?retryWrites=true&w=majority',
-// { useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-// app.listen(3000,() => {
-//     console.log("App listening on port 3000")
-// })
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error',(error)=>console.log(error));
+db.once('open',()=>console.log('Database Already'))
+mongoose.connect('mongodb+srv://nicekrubma10:kulab12345@cluster0.uqjxafb.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+app.listen(3000,() => {
+    console.log("App listening on port 3000")
+})
---
+++ cafe/src/Home.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const Home = () => (
+  <div>
+    <h2>Welcome to the Home Page</h2>
+    <p>This is the content you see when you are not logged in.</p>
+  </div>
+);  
+
+export default App;
