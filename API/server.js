f6a4776d884c1b2918838622d1eb8ea139f98729
ThapananKulab <nicekrubma123@gmail.com>
2024-02-09 21:38:24 +0700

    Update server.js

--- API/server.js
+++ API/server.js
@@ -5,7 +5,7 @@ var app = express();
 var jsonParser = bodyParser.json()
 // var urlencodedParser = bodyParser.urlencoded({ extended: false });
 const mongoose = require('mongoose');
-const User = require('./models/User.js')
+const User = require('./models/User')
 
 
 // var cookieParser = require('cookie-parser')
