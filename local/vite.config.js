b6c4f8a3d85b06ff3f120cae4d423fa00d726b11
ThapananKulab <nicekrubma123@gmail.com>
2024-02-13 20:09:45 +0700

    update

--- local/vite.config.js
+++ local/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3333',
+      '/api': 'https://cafe-project-server11.onrender.com',
     },
   },
 })
