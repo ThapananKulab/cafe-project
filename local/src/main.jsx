4b6d2b2543c7211f07cafccdca982997bdf873b6
ThapananKulab <nicekrubma123@gmail.com>
2024-02-13 19:49:56 +0700

    Update main.jsx

--- local/src/main.jsx
+++ local/src/main.jsx
@@ -8,7 +8,7 @@ import Dashboard from './Dashboard'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Dashboard />} />
       <Route path="/Dashboard" element={<Dashboard />} />
     </Routes>
   </BrowserRouter>
