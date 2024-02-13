521a557308a1663bf355011b0b5b34070f59e778
ThapananKulab <nicekrubma123@gmail.com>
2024-02-13 22:03:07 +0700

    create product

--- local/src/Product.jsx
+++ local/src/Product.jsx
@@ -1,29 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
+import Swal from 'sweetalert2';
 
 function Example() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-useEffect(() => {
-  fetch('https://cafe-project-server11.onrender.com/api/products')
-    .then(response => response.json())
-    .then(data => {
-      setProducts(data);
-      setLoading(false);
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-      setError('Error fetching data. Please try again.');
-      setLoading(false);
-    });
-}, []);
-
+  useEffect(() => {
+    fetch('https://cafe-project-server11.onrender.com/api/products')
+      .then(response => response.json())
+      .then(data => {
+        setProducts(data);
+        setLoading(false);
+        // Close the loading spinner once data is loaded
+        Swal.close();
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setError('Error fetching data. Please try again.');
+        setLoading(false);
+        Swal.close();
+      });
+  }, []);
 
   if (loading) {
-    return <p>Loading...</p>;
+    Swal.showLoading();
   }
 
   if (error) {
@@ -32,17 +35,13 @@ useEffect(() => {
 
   return (
     <div>
-      <h1>
-        Example heading
-        <Badge bg="secondary" as={Button}>
-          New
-        </Badge>
-      </h1>
+      <h1>รายชื่อเมนู</h1>
 
       {/* Display the products */}
       <ul>
         {products.map(product => (
-          <li key={product._id}>{product.productname}</li>
+          <li key={product._id}>
+            {`ID: ${product._id} - Name: ${product.productname}`}</li>
         ))}
       </ul>
     </div>
