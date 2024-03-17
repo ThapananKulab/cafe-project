1b20562235f46b9e3cf32570366920d5fa3e1090
ThapananKulab <nicekrubma123@gmail.com>
2024-03-18 01:18:38 +0700

    update

--- API/routes/menus.js
+++ API/routes/menus.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const Menu = require('../models/menu.js')
 
-//
+////
 router.post('/addMenu', async (req, res) => {
   const menuItem = new Menu({
     name: req.body.name,
